bbd5282b1e619b625357b1b694bc900dc75dd854
VDmitryO <vshk97@mail.ru>
2017-12-29 15:37:47 +0300

    pushkin 6

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :rails_env, 'production'
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "92.53.91.100", user: "deployer", roles: %w{app db web}, my_property: :my_value
+server "92.53.91.100", user: "deployer", roles: %w{app db web}, primary: :true
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
